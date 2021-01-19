589750ab93bd229d3c2105effb8a9ae8c8e9a233
Jacob Clark <jacob.jh.clark@googlemail.com>
2021-01-19 20:54:45 +0000

    Refactoring

--- src/Lib.hs
+++ src/Lib.hs
@@ -5,25 +5,24 @@ module Lib
 import Data.List (intersperse, intercalate)
 
 newtype Polynomial = Polynomial [Int] deriving Show
+newtype Point = Point (Int, Int) deriving Show
 
 polypend :: Int -> Polynomial -> Polynomial
 polypend x (Polynomial y) = Polynomial (x : y)
 
 add :: Polynomial -> Polynomial -> Polynomial
-add (Polynomial []) (Polynomial []) = Polynomial []
 add (Polynomial []) ys = ys
 add xs (Polynomial []) = xs
-add (Polynomial (x:xs)) (Polynomial (y:ys)) = add (Polynomial((x+y):xs)) (Polynomial ys)
+add (Polynomial (x:xs)) (Polynomial (y:ys)) = polypend (x+y) (add (Polynomial xs) (Polynomial ys))
 
 scale :: Int -> Polynomial -> Polynomial
 scale a (Polynomial xs) = Polynomial (map (a*) xs)
 
 multiply :: Polynomial -> Polynomial -> Polynomial
-multiply (Polynomial ys) (Polynomial xs)  = foldr (\x acc -> do 
-  add 
-    (scale x (Polynomial ys)) (polypend 0 acc)) 
-    (Polynomial []) 
-    xs
+multiply (Polynomial ys) (Polynomial xs) = foldr 
+  (\x acc -> add (scale x (Polynomial ys)) (polypend 0 acc))
+  (Polynomial []) 
+  xs
 
 monomialWithPosition :: Polynomial -> Int -> [(Int, Int)]
 monomialWithPosition (Polynomial []) _ = []
@@ -47,13 +46,23 @@ format (coefficient, exponent) = show coefficient ++ "x^" ++ show exponent
 stringulate :: [[Char]] -> [Char]
 stringulate = intercalate "" . intersperse " + "
 
+-- singleTerm :: [Point] -> Int -> Int
+-- singleTerm points i = where
+--   let term = Polynomial [1]
+--   let (Point (x, y)) = points !! i
+
+--   x
+
+
 polynomials :: IO ()
 polynomials = do
   let p1 = Polynomial [1,2,3]
   let p2 = Polynomial [1,2,3]
-  let p3 = add p1 p2
-  print $ evaluate p3 5
-  print . stringulate . map format $ monomialWithPosition p3 (polynomialLength p3 - 1)
+  let p3 = add (Polynomial [1,2,3]) (Polynomial [1,2,3])
+  print . stringulate . map format . monomialWithPosition p1 $ polynomialLength p1 - 1
+  print . stringulate . map format . monomialWithPosition p2 $ polynomialLength p2 - 1
+  print . stringulate . map format . monomialWithPosition (multiply p1 p2) $ polynomialLength (multiply p1 p2) - 1
+  print $ multiply p1 p2
 
 -- -- https://github.com/hashanp/haskell-projects/blob/master/Polynomial.hs
 -- -- http://hackage.haskell.org/package/dsp-0.2.1/docs/src/Polynomial-Basic.html
